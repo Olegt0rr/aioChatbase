dd4d4df0a439cb6af6029eea68de31990683d8fa
Oleg Abramov <t0rr@mail.ru>
2018-06-22 01:17:27 +0300

    add setup file

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="aioChatbase",
+    version="0.0.1",
+    author="Oleg Abramov",
+    author_email="1282524@gmail.com",
+    description="Asyncio Chatbase API library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Olegt0rr/aioChatbase",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
