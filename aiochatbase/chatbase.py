6f0cd7e2ce5e40e750636aed6a956ff767e2d439
Oleg Abramov <t0rr@mail.ru>
2018-06-22 12:45:27 +0300

    Docs edit

--- aiochatbase/chatbase.py
+++ aiochatbase/chatbase.py
@@ -1,5 +1,5 @@
-from datetime import datetime
 import logging
+from datetime import datetime
 
 from .types import Message, MessageTypes, Click, Event
 
@@ -9,9 +9,10 @@ logger = logging.getLogger(f'chatbase')
 class Chatbase:
     def __init__(self, api_key, platform):
         """
-        :param api_key:
+        :param api_key: Chatbase API token key
         :type api_key: str
-        :param platform:
+
+        :param platform: your chat bot platform (Telegram, Facebook, Android, etc.)
         :type platform: str
         """
         self.api_key = api_key
@@ -21,6 +22,8 @@ class Chatbase:
                                session_id=None, message_type=MessageTypes.USER,
                                time_stamp=datetime.now().timestamp()):
         """
+        Register message
+
         :param user_id: chatbot user id
         :type user_id: str
 
@@ -65,11 +68,18 @@ class Chatbase:
 
     async def register_click(self, url, user_id=None, version=None):
         """
+        Register clicked URL
 
-        :param url:
-        :param user_id:
-        :param version:
-        :return: True if OK
+        :param url: clicked URL
+        :type url: str
+
+        :param user_id: chatbot user id
+        :type user_id: str
+
+        :param version: fill to track versions of your code
+        :type version: str
+
+        :return: True if result is OK
         :rtype bool
         """
         click = Click(self.api_key, url, self.platform, user_id=user_id, version=version)
@@ -77,13 +87,34 @@ class Chatbase:
         logger.debug(f"Registered {self.platform} click from user {user_id} to url '{url}'. ")
         return result
 
-    async def register_event(self, user_id, intent, time_stamp=datetime.now().timestamp(), platform=None,
+    async def register_event(self, user_id, intent, time_stamp=datetime.now().timestamp(),
                              version=None, properties=None):
+        """
+        Register event
+
+        :param user_id: chatbot user id
+        :type user_id: str
+
+        :param intent: chatbot user intention
+        :type intent: str
+
+        :param time_stamp: milliseconds since the UNIX epoch, used to sequence messages.
+        :type time_stamp: int
+
+        :param version: fill to track versions of your code
+        :type version: str
+
+        :param properties: misc properties
+        :type properties: dict
+
+        :return: True if result is OK
+        :rtype: bool
+        """
         event = Event(api_key=self.api_key,
                       user_id=user_id,
                       intent=intent,
                       timestamp_millis=int(time_stamp * 1000),
-                      platform=platform,
+                      platform=self.platform,
                       version=version,
                       properties=properties)
         result = await event.send()
