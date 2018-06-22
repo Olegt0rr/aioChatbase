23a7b87dc05afc2d7d5b675e9cb0273c3c1f5271
Oleg Abramov <t0rr@mail.ru>
2018-06-22 15:22:54 +0300

    Add task mode #4

--- aiochatbase/chatbase.py
+++ aiochatbase/chatbase.py
@@ -1,3 +1,4 @@
+import asyncio
 import logging
 from datetime import datetime
 
@@ -7,16 +8,21 @@ logger = logging.getLogger(f'chatbase')
 
 
 class Chatbase:
-    def __init__(self, api_key, platform):
+    def __init__(self, api_key, platform, task_mode=False):
         """
         :param api_key: Chatbase API token key
         :type api_key: str
 
         :param platform: your chat bot platform (Telegram, Facebook, Android, etc.)
         :type platform: str
+
+        :param task_mode: returns asyncio.Task in register_* methods
+        :type task_mode: bool
         """
+
         self.api_key = api_key
         self.platform = platform
+        self.task_mode = task_mode
 
     async def prepare_message(self, user_id, intent=None, message=None, not_handled=None, version=None,
                               session_id=None, message_type=MessageTypes.USER,
@@ -46,7 +52,7 @@ class Chatbase:
         :type message_type: str
 
         :param time_stamp: seconds since the UNIX epoch, used to sequence messages.
-        :type time_stamp: int
+        :type time_stamp: float
 
         :return: Chatbase message
         :rtype: Message
@@ -64,37 +70,57 @@ class Chatbase:
 
     async def register_message(self, user_id, intent=None, message=None, not_handled=None, version=None,
                                session_id=None, message_type=MessageTypes.USER,
-                               time_stamp=datetime.now().timestamp()):
+                               time_stamp=datetime.now().timestamp(), task=None):
         """
-        Register message
+         Register message
 
-        :param user_id: chatbot user id
-        :type user_id: str
+         :param user_id: chatbot user id
+         :type user_id: str
 
-        :param intent: chatbot user intention
-        :type intent: str
+         :param intent: chatbot user intention
+         :type intent: str
 
-        :param message: user full message
-        :type message: str
+         :param message: user full message
+         :type message: str
 
-        :param not_handled: True if your bot don't understand user intention
-        :type not_handled: bool
+         :param not_handled: True if your bot don't understand user intention
+         :type not_handled: bool
 
-        :param version: fill to track versions of your code
-        :type version: str
+         :param version: fill to track versions of your code
+         :type version: str
 
-        :param session_id: fill to track your own custom sessions
-        :type session_id: str
+         :param session_id: fill to track your own custom sessions
+         :type session_id: str
 
-        :param message_type: "user" or "agent" (aka your chatbot)
-        :type message_type: str
+         :param message_type: "user" or "agent" (aka your chatbot)
+         :type message_type: str
 
-        :param time_stamp: seconds since the UNIX epoch, used to sequence messages.
-        :type time_stamp: int
+         :param time_stamp: seconds since the UNIX epoch, used to sequence messages.
+         :type time_stamp: int
 
-        :return: Chatbase message id
-        :rtype: str
-        """
+         :param task: Returns aio.Task if True, returns result if False, default value if None
+         :type task: bool
+
+         :return: Chatbase message id
+         :rtype: str
+         """
+
+        coroutine = self._register_message(user_id=user_id, intent=intent, message=message, not_handled=not_handled,
+                                           version=version, session_id=session_id, message_type=message_type,
+                                           time_stamp=time_stamp)
+        if isinstance(task, bool):
+            if not task:
+                return await coroutine
+            return asyncio.ensure_future(coroutine)
+
+        if self.task_mode:
+            return asyncio.ensure_future(coroutine)
+
+        return await coroutine
+
+    async def _register_message(self, user_id, intent=None, message=None, not_handled=None, version=None,
+                                session_id=None, message_type=MessageTypes.USER,
+                                time_stamp=datetime.now().timestamp()):
         message = await self.prepare_message(user_id, intent=intent, message=message, not_handled=not_handled,
                                              version=version, session_id=session_id, message_type=message_type,
                                              time_stamp=time_stamp)
@@ -116,7 +142,7 @@ class Chatbase:
         logger.debug(f"Registered {self.platform} messages: {msgs_id_list}")
         return msgs_id_list
 
-    async def register_click(self, url, user_id=None, version=None):
+    async def register_click(self, url, user_id=None, version=None, task=None):
         """
         Register clicked URL
 
@@ -129,16 +155,33 @@ class Chatbase:
         :param version: fill to track versions of your code
         :type version: str
 
+        :param task: Returns aio.Task if True, returns result if False, default value if None
+        :type task: bool
+
         :return: True if result is OK
         :rtype bool
         """
+
+        coroutine = self._register_click(url=url, user_id=user_id, version=version)
+
+        if isinstance(task, bool):
+            if not task:
+                return await coroutine
+            return asyncio.ensure_future(coroutine)
+
+        if self.task_mode:
+            return asyncio.ensure_future(coroutine)
+
+        return await coroutine
+
+    async def _register_click(self, url, user_id=None, version=None):
         click = Click(self.api_key, url, self.platform, user_id=user_id, version=version)
         result = await click.send()
         logger.debug(f"Registered {self.platform} click from user {user_id} to url '{url}'. ")
         return result
 
-    async def register_event(self, user_id, intent, time_stamp=datetime.now().timestamp(),
-                             version=None, properties=None):
+    async def register_event(self, user_id, intent, time_stamp=datetime.now().timestamp(), version=None,
+                             properties=None, task=None):
         """
         Register event
 
@@ -148,8 +191,8 @@ class Chatbase:
         :param intent: chatbot user intention
         :type intent: str
 
-        :param time_stamp: milliseconds since the UNIX epoch, used to sequence messages.
-        :type time_stamp: int
+        :param time_stamp: seconds since the UNIX epoch, used to sequence messages.
+        :type time_stamp: float
 
         :param version: fill to track versions of your code
         :type version: str
@@ -157,9 +200,27 @@ class Chatbase:
         :param properties: misc properties
         :type properties: dict
 
+        :param task: Returns aio.Task if True, returns result if False, default value if None
+        :type task: bool
+
         :return: True if result is OK
         :rtype: bool
         """
+        coroutine = self._register_event(user_id=user_id, intent=intent, time_stamp=time_stamp, version=version,
+                                         properties=properties)
+
+        if isinstance(task, bool):
+            if not task:
+                return await coroutine
+            return asyncio.ensure_future(coroutine)
+
+        if self.task_mode:
+            return asyncio.ensure_future(coroutine)
+
+        return await coroutine
+
+    async def _register_event(self, user_id, intent, time_stamp=datetime.now().timestamp(),
+                              version=None, properties=None):
         event = Event(api_key=self.api_key,
                       user_id=user_id,
                       intent=intent,
---
+++ examples/task_feature.py
@@ -0,0 +1,25 @@
+import asyncio
+
+from aiochatbase import Chatbase
+
+API_KEY = 'paste-your-api-key-token-here'
+PLATFORM = 'Telegram'
+
+loop = asyncio.get_event_loop()
+cb = Chatbase(api_key=API_KEY, platform=PLATFORM, task_mode=True)
+
+
+async def background_send_handled_message():
+    print('Registering message as background task...')
+    task: asyncio.Task = await cb.register_message(user_id='512345678', intent='test message 2')
+
+    print("Let's calc 2+2, while cb registering message...")
+    s = 2 + 2
+    print(f'2 + 2 = {s}')
+    await asyncio.sleep(2)
+
+    print(f"Let's get Chatbase message id: {task.result()}")
+
+
+loop.run_until_complete(background_send_handled_message())
+loop.close()
