f4e65cbf8020270a9029de4a6bf51545c39114fd
Oleg Abramov <t0rr@mail.ru>
2018-07-21 10:31:50 +0300

    ujson support

---
+++ aiochatbase/utils/json.py
@@ -0,0 +1,31 @@
+"""
+ujson support module
+
+"""
+
+import json
+
+try:
+    import ujson
+
+except ImportError:
+    ujson = None
+
+_use_ujson = True if ujson else False
+
+
+def disable_ujson():
+    global _use_ujson
+    _use_ujson = False
+
+
+def dumps(data):
+    if _use_ujson:
+        return ujson.dumps(data)
+    return json.dumps(data)
+
+
+def loads(data):
+    if _use_ujson:
+        return ujson.loads(data)
+    return json.loads(data)
