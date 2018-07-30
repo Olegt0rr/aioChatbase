e33c5bb37d997e8962a3e609d9eb0c91b760fb19
t0rr <t0rr@mail.ru>
2018-07-30 14:02:11 +0300

    version up

--- aiochatbase/__init__.py
+++ aiochatbase/__init__.py
@@ -1,2 +1,2 @@
 from .chatbase import Chatbase
-__version__ = '0.0.7'
+__version__ = '0.0.8'
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="aioChatbase",
-    version="0.0.7",
+    version="0.0.8",
     author="Oleg Abramov",
     author_email="1282524@gmail.com",
     description="Asyncio Chatbase API library",
