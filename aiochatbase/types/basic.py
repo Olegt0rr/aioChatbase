471f31a1bb3c9bf5e703a8ddfd6044201624f5aa
Oleg A <t0rr@mail.ru>
2018-11-21 08:34:22 +0300

    detailed unknown exception logging

--- aiochatbase/types/basic.py
+++ aiochatbase/types/basic.py
@@ -19,20 +19,17 @@ class BasicChatbaseObject:
         """
 
         async with session.post(self._api_url, data=self.to_json(), headers=self._content_type) as resp:
+            response_json = await resp()
+            response_dict = json.loads(response_json)
+
             if resp.status == 200:
-                response_json = await resp.text()
-                response_dict = json.loads(response_json)
                 logger.debug(f'Resp status: {resp.status}, resp text: {response_json}')
                 return response_dict
 
             if resp.status == 400:
-                response_json = await resp.text()
-                response_dict = json.loads(response_json)
                 error_text = response_dict.get('reason')
-
                 if error_text == "Error fetching parameter 'api_key': Missing or invalid field(s): 'api_key'":
                     raise InvalidApiKey()
-
                 raise ChatbaseException(error_text)
 
-            raise ChatbaseException('Unknown response')
+            raise ChatbaseException(f'Unknown response: {resp.status} {response_dict}')
